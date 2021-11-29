25ba39d6cb7827dca058c8ec7a1d1ea4ab9bb28a
Pedro Góis <77770841+pedrohgois@users.noreply.github.com>
2021-11-29 09:36:37 -0300

    Add files via upload

---
+++ js/menu_minimizado.js
@@ -0,0 +1,14 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+  $(".sidenav a").click(  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  })
+
+  
+
